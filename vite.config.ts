c99b0d74b2e32c033773d4fac69fc5cd4a8f4359
AdrianRC <adrian.rosario.codik@gmail.com>
2021-04-14 00:15:49 -0400

    feat: configure lib building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/DatoImage.ts"),
+      name: "DatoCMS-Image",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
